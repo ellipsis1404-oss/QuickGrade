ab67dc19f725d730db4b8753d02929a5b71424e0
ellipsis1404-oss <ellipsis1404@gmail.com>
2025-08-24 08:37:30 +0700

    FEAT: Add health check URL for Render

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse 
 
 urlpatterns = [
+    path("health/", lambda request: HttpResponse("OK"), name="health_check"),
     path('admin/', admin.site.urls),
     # This line tells Django that any URL starting with 'api/'
     # should be handled by the 'api' app's urls.
